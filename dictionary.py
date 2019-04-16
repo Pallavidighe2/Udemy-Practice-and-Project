329c9fce0b20c23d001df18da3c286cbdfe65322
pallavi <pallavidighe2@gmail.com>
2019-04-16 11:47:40 +0530

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,36 @@
+# people = {"Pallavi":[1,2,3,"gh"] , "babasaheb":30}
+# print(people["Pallavi"])
+
+fruits = {1:"Mango", 2: "BAnana", 3:"Orange"}
+# print(fruits)
+# print(fruits[1])
+# print(1 in fruits)
+# print(4 in fruits)
+print(fruits.get(1))
+# print(fruits.get(5))
+print(fruits.get(5,"Kay 5 is not presenrt in the dictionary"))
+
+
+# ASSIGNMENT:
+
+# product= {"table":500,"chair":200,"Pen":10, "Pensil":2,"book":1000}
+# print(product["book"])
+#
+# value=input("enter product name : ")
+#
+# if (value in product):
+#     print(product.get(value))
+#
+# else:
+#     print("produc not found")
+
+
+
+# element ={"apple":50,"banana":20,"mango":100,"orange":10}
+#
+# value=input("enter name of product : ")
+#
+# if value in element:
+#     print(element.get(value))
+# else:
+#     print("elemnt not present : ")
