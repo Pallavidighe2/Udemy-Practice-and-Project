9fad72563ef3fab12a191548b08b784bb3f022ef
pallavi <pallavidighe2@gmail.com>
2019-04-16 11:53:03 +0530

    While loop

---
+++ While loop.py
@@ -0,0 +1,40 @@
+# x = -10
+# while x<=5:
+#     print("PAllavi")
+#
+#     x=x+1
+
+
+# List = ["Apple","Mango","Biscuit","Cake","Grapes"]
+# print(List)
+# print(List[2])
+# List.append("Chocolate")
+# print(List)
+# List.insert(3,"TT")
+# print(List)
+
+# for X in range(1,6):
+#     print("I am Engineer")
+# #
+#
+# def square():
+#
+#     for i in range(1,11):
+#         # i*=i
+#         print(i*i)
+#
+# square()
+
+# x=0
+# while x<5:
+#     print("aa")
+#     x+=1
+
+import random
+
+a=random.randint(1,10)
+
+
+for i in range (5):
+    a = random.randint(1, 10)
+    print(a)
