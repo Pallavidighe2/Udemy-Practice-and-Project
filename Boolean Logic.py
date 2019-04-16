e6d3f1276ac2fbf1288987887eac32a9e4478f8d
pallavi <pallavidighe2@gmail.com>
2019-04-16 11:45:57 +0530

    Boolean Logic

---
+++ Boolean Logic.py
@@ -0,0 +1,15 @@
+# username=input("enter user name: ")
+# password=input("Enter password")
+#
+# if username=="Pallavi" and password=="Pallavi111":
+#     print("Valid User")
+# else:
+#     print("Invalid User")
+
+username=1234
+password=123
+
+if username==12345 or password==123:
+    print("Valid User")
+else:
+     print("Invalid User")
