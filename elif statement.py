102510b1b9e76397c13037e2a67d08b01c5f01b2
pallavi <pallavidighe2@gmail.com>
2019-04-16 11:48:00 +0530

    elif statement

---
+++ elif statement.py
@@ -0,0 +1,20 @@
+# Marks = int(input("Enter your Marks : "))
+# if Marks >= 90:
+#     print("Grade A")
+# elif Marks >= 70:
+#     print("Grade B")
+# elif Marks >= 60:
+#     print("Grade C")
+# elif Marks < 60:
+#     print("Grade D")
+
+Marks = int(input("Enter your Marks : "))
+if Marks >= 90:
+    print("Grade A")
+elif 60<Marks<70:
+    print("Grade C")
+elif 70 < Marks < 90:
+    print("grade B")
+
+
+
