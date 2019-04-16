454e0361760641228adac5faf6fb864242149bec
pallavi <pallavidighe2@gmail.com>
2019-04-16 11:48:19 +0530

    Exception Handling

---
+++ Exception Handling.py
@@ -0,0 +1,33 @@
+# try:
+#     def function1(a,b):
+#         print(a/b)
+#
+#     function1(4,2)
+#
+# except ZeroDivisionError:
+#     print("hello")
+#
+# finally:
+#     print("it is going to execute no matter what")
+# try:
+#     def division(a,b):
+#         # a=5
+#         # b=0
+#         print(a/b)
+#     division(5,0)
+# except :
+#     print("do division")
+
+try:
+
+    def dev(a,b):
+        print(a/b)
+    dev(5,2)
+
+except:
+    print("Division by zero")
+
+finally:
+    print("Pallavi is a good girl")
+
+
