86d4c03a20a81fd58c8abb74ec2af514e9b97dc9
pallavi <pallavidighe2@gmail.com>
2019-04-16 11:52:17 +0530

    string

---
+++ string.py
@@ -0,0 +1,35 @@
+# print("Pallavi birthday is : {}" .format("18 nov"))
+#
+# fruits = ["Mango" ,"Apple"]
+# print("Pallavi ".join(fruits[1]))
+
+# string= "Pallavi is good girl"
+# print(string .replace("good"," beautiful"))
+#
+# print("Pallavi".upper())
+# print("Pallavi".lower())
+#
+# print(min(1,2,8,9))
+# print(max(4,8,3,1))
+# print(abs(-4.5))
+
+
+string="123"
+
+string1=list(reversed(string))
+print(string1)
+if string==string1:
+    print("palindrome")
+else:
+    print("aaa")
+
+number1 = "123"
+number2 =list(reversed(number1))
+# print(number2)
+
+if number1==number2:
+    print("palindrome")
+else:
+    print("not palindrome")
+
+
