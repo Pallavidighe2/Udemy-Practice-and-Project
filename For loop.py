28c3ff337e9e74c190eb850aac6dec2b35bda4f6
pallavi <pallavidighe2@gmail.com>
2019-04-16 11:48:46 +0530

    for loop

---
+++ For loop.py
@@ -0,0 +1,8 @@
+# for x in range(1,5):
+#     print("Pallavi")
+# Fruits=["Apple","Mango","Banana","Guava","Watermelon"]
+#
+# for x in Fruits:
+#     print("x")
+for i in range(1,21):
+    print(i)
